5518cb889ae396aa5d517ba9b61d90bc6ac8050c
Shivam Rajput <shivmt880@gmail.com>
2026-08-28 00:10:08 +0530

    js-progressive bar

---
+++ day7(time-and-interval)/day7(timers&iIntervals).js
@@ -0,0 +1,21 @@
+/* setTimeout(()=>{
+console.log("y aa gya m");
+},5000)
+
+const clock = setInterval(()=>{
+    console.log("m har bar 8 sec m chalunga");
+},8000);
+ 
+ clearInterval(clock);
+
+console.log("timer aane wala h");
+*/
+    let count = 1;
+
+let counter = setInterval(()=>{
+    console.log(count);
+    count++;
+if(count > 9){
+    clearInterval(counter);
+}
+ },2000);
